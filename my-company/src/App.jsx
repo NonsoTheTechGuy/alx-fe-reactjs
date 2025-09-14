2e7c9b21405507afadf48bd31aa850aa50f91dad
Kalu Jonah Chinonso <kaluchinonso18@gmail.com>
2025-09-14 04:45:52 +0100

    Objective: Created a four-page company website using React.

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from '.src/components/Navbar';
+import Navbar from './Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Services from './components/Services';
