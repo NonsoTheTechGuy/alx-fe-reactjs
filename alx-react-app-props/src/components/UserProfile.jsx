fb1bd755732a6e77735fe2f992c1a96e9e7ce3f6
Kalu Jonah Chinonso <kaluchinonso18@gmail.com>
2025-09-14 03:14:28 +0100

    Desc: Refactoring an existing React application that uses prop drilling to manage user data across multiple components, also implementing the Context API to streamline data flow within the application.

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,18 +1,6 @@
-//  const UserProfile = (props) => {
-//    return (
-//      <div>
-//        <h2>{props.name}</h2>
-//        <p>Age: {props.age}</p>
-//        <p>Bio: {props.bio}</p>
-//      </div>
-//    );
-// };
- 
-// export default UserProfile;
 import React, { useContext } from 'react';
 import UserContext from './UserContext';
 
-// ...existing code...
 function UserProfile() {
   const userData = useContext(UserContext);
 
@@ -26,3 +14,15 @@ function UserProfile() {
 }
 
 export default UserProfile;
+
+//  const UserProfile = (props) => {
+//    return (
+//      <div>
+//        <h2>{props.name}</h2>
+//        <p>Age: {props.age}</p>
+//        <p>Bio: {props.bio}</p>
+//      </div>
+//    );
+// };
+ 
+// export default UserProfile;
